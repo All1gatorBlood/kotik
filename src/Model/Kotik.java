ec82f119787d131ec156124be6057f8e5f9dfb42
Андрей <@192.168.0.13>
2021-06-16 11:38:53 +0300

    next1

--- src/Model/Kotik.java
+++ src/Model/Kotik.java
@@ -1,9 +1,71 @@
 package Model;
 
 public class Kotik {
-	int hunger=10;
-	int prettiness,weight;
-	String name, meow;
+	private int hunger=10;
+	private int prettiness,weight;
+	private String name, meow;
+	private int objectC=0;
+	
+	
+	//get and set
+	public int getHunger() {
+		return hunger;
+	}
+
+	public void setHunger(int hunger) {
+		this.hunger = hunger;
+	}
+
+	public int getPrettiness() {
+		return prettiness;
+	}
+
+	public void setPrettiness(int prettiness) {
+		this.prettiness = prettiness;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMeow() {
+		return meow;
+	}
+
+	public void setMeow(String meow) {
+		this.meow = meow;
+	}
+
+	public int getObjectC() {
+		return objectC;
+	}
+
+	public void setObjectC(int objectC) {
+		this.objectC = objectC;
+	}
+
+	//Construct
+	public Kotik(int prettiness, int weight, String name, String meow) {
+		super();
+		this.prettiness = prettiness;
+		this.weight = weight;
+		this.name = name;
+		this.meow = meow;
+		objectC++;
+	}
+	
 	
 	//методы
 	private void liveAnotherDay() {
@@ -16,56 +78,62 @@ public class Kotik {
 		case 4:callingSatan(); break;
 		case 5:chaseDemon(); break;
 		}
+
 	}
 
 	}
-	private void play() {
+	public void play() {
 		if(hunger<0) {
 			System.out.println(name+"Hungry");
 			eat();
 		}else {
 				System.out.println(name+"playing, don't scare him");
+				hunger--;
 			}
 		}
 
-	private void sleep() {
+	public void sleep() {
 		if(hunger<0) {
 			System.out.println(name+"Hungry");
 			eat();
 		}else {
 				System.out.println(name+"sleeping, bless good");
+				hunger--;
 			}
 
 	}
-	private void angry() {
+	public void angry() {
 		if(hunger<0) {
 			System.out.println(name+"Hungry");
 			eat();
 		}else {
 				System.out.println(name+"See, i feel fear");
+				hunger--;
 			}
 
 	}
-	private void callingSatan() {
+	public void callingSatan() {
 		if(hunger<0) {
 			System.out.println(name+"Hungry");
 			eat();
 		}else {
 				System.out.println(name+"draws a pentagram");
+				hunger--;
 			}
 
 	}
-	private void chaseDemon() {
+	public void chaseDemon() {
 		if(hunger<0) {
 			System.out.println(name+"Hungry");
 			eat();
 		}else {
 				System.out.println(name+"expels the demon");
+				hunger--;
 			}
 
 	}
 	
-	private void eat() {
+	public void eat() {
 		hunger+=10;
 		System.out.println(name+"not hungry anymore, dont be scared");
 
--- src/src/Application.java
+++ src/src/Application.java
@@ -1,7 +1,10 @@
 package src;
+import Model.Kotik;
 
 public class Application {
 	public static void main(String[] args) {
+		Kotik cat = new Kotik(10,10,"Мякишек","RRRRRRR");
+		
 		
 	}
 
